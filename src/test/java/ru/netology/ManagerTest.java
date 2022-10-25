345b05f6cf56526a692f28d2491c3917c1aabc19
unicred666 <varchyn207@gmail.com>
2022-10-25 22:38:59 +0300

    edit ManagerTest2

--- src/test/java/ru/netology/ManagerTest.java
+++ src/test/java/ru/netology/ManagerTest.java
@@ -4,6 +4,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ManagerTest {
+    Manager manager = new Manager();
+    String movie1 = "movie1";
+    String movie2 = "movie2";
+    String movie3 = "movie3";
+    String movie4 = "movie4";
+    String movie5 = "movie5";
 
     @Test
     public void test1() {
@@ -13,7 +19,6 @@ public class ManagerTest {
         String movie4 = "movie4";
         String movie5 = "movie5";
 
-        Manager manager = new Manager();
         manager.add(movie1);
         manager.add(movie2);
         manager.add(movie3);
@@ -66,17 +71,15 @@ public class ManagerTest {
         String movie2 = "movie2";
         String movie3 = "movie3";
         String movie4 = "movie4";
-        ;
-
-        Manager manager = new Manager();
 
         manager.add(movie1);
         manager.add(movie2);
         manager.add(movie3);
         manager.add(movie4);
 
-        String[] expected = {"movie4", "movie3", "movie2", "movie1" };
+        String[] expected = {"movie4", "movie3", "movie2", "movie1"};
         String[] actual = manager.findLast();
+
         Assertions.assertArrayEquals(expected, actual);
     }
 
@@ -90,8 +93,6 @@ public class ManagerTest {
         String movie5 = "movie5";
         String movie6 = "movie6";
 
-        Manager manager = new Manager();
-
         manager.add(movie1);
         manager.add(movie2);
         manager.add(movie3);
@@ -99,8 +100,9 @@ public class ManagerTest {
         manager.add(movie5);
         manager.add(movie6);
 
-        String[] expected = {"movie6", "movie5", "movie4", "movie3", "movie2", "movie1" };
+        String[] expected = {"movie6", "movie5", "movie4", "movie3", "movie2", "movie1"};
         String[] actual = manager.findLast();
+
         Assertions.assertArrayEquals(expected, actual);
     }
 
@@ -114,5 +116,164 @@ public class ManagerTest {
         String[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    //вывод фильмов в обратном порядке добавления в количестве меньше лимита когда добавленых фильмов меньше лимита (нижняя граница)
+    public void outputMovieLinitOnMin() {
+
+        String[] expected = {};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    // вывод фильмов в обратном порядке добавления в количестве меньше лимита когда добавленых фильмов меньше лимита (нижняя граница + 1) (лимит по умолчанию)
+    public void outputMovieLimitOnMin1() {
+        manager.add("movie1");
+        String[] expected = {"movie1"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    // вывод фильмов в обратном порядке добавления в количестве меньше лимита когда добавленых фильмов меньше лимита (лимит по умолчанию)
+    public void outputMovieLimitOn() {
+        manager.add("movie1");
+        manager.add("movie2");
+        manager.add("movie3");
+        manager.add("movie4");
+
+        String[] expected = {"movie4", "movie3", "movie2", "movie1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    // вывод фильмов в обратном порядке добавления в количестве меньше лимита когда добавленых фильмов меньше лимита (нижняя граница + 1) (лимит установлен)
+    public void outputMovieLimitOffMin1() {
+        Manager manager = new Manager(3);
+        manager.add("movie1");
+
+        String[] expected = {"movie1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    // вывод фильмов в обратном порядке добавления в количестве меньше лимита когда добавленых фильмов больше лимита (верхняя граница лимита +1) (лимит установлен)
+    public void outputMovieLimitOffMax2() {
+        Manager manager = new Manager(4);
+        manager.add("movie1");
+        manager.add("movie2");
+        manager.add("movie3");
+        manager.add("movie4");
+        manager.add("movie5");
+
+        String[] expected = {"movie5", "movie4", "movie3", "movie2"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    //  вывод фильмов в обратном порядке добавления в количестве меньше лимита когда добавленых фильмов меньше лимита (верхняя граница лимита - 1) (лимит по умолчанию)
+    public void outputMovieLimitOnMax0() {
+        manager.add("movie1");
+        manager.add("movie2");
+        manager.add("movie3");
+        manager.add("movie4");
+        manager.add("movie5");
+
+        String[] expected = {"movie5", "movie4", "movie3", "movie2", "movie1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    // вывод фильмов в обратном порядке добавления в количестве меньше лимита когда добавленых фильмов больше лимита (лимит по умолчанию)
+    public void outputMovieLimitOnMaxMax() {
+        Manager manager = new Manager(3);
+        manager.add("movie1");
+        manager.add("movie2");
+        manager.add("movie3");
+        manager.add("movie4");
+        manager.add("movie5");
+
+        String[] expected = {"movie5", "movie4", "movie3"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    // вывод фильмов в обратном порядке добавления в количестве меньше лимита когда добавленых фильмов меньше лимита (лимит установлен)
+    public void outputMovieLimitOff() {
+        Manager manager = new Manager(10);
+        manager.add("movie1");
+        manager.add("movie2");
+        manager.add("movie3");
+        manager.add("movie4");
+        manager.add("movie5");
+
+        String[] expected = {"movie5", "movie4", "movie3", "movie2", "movie1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    //  вывод фильмов в обратном порядке добавления в количестве меньше лимита когда добавленых фильмов меньше лимита (верхняя граница лимита - 1) (лимит установлен)
+    public void outputMovieLimitOffMax0() {
+        Manager manager = new Manager(5);
+        manager.add("movie1");
+        manager.add("movie2");
+        manager.add("movie3");
+        manager.add("movie4");
+
+        String[] expected = {"movie4", "movie3", "movie2", "movie1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    // вывод фильмов в обратном порядке добавления в количестве меньше лимита когда добавленых фильмов равно лимиту (верхняя граница лимита) (лимит установлен)
+    public void outputMovieLimitOffMax1() {
+        Manager manager = new Manager(5);
+        manager.add("movie1");
+        manager.add("movie2");
+        manager.add("movie3");
+        manager.add("movie4");
+        manager.add("movie5");
+
+        String[] expected = {"movie5", "movie4", "movie3", "movie2", "movie1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    // вывод фильмов в обратном порядке добавления в количестве меньше лимита когда добавленых фильмов больше лимита (лимит установлен)
+    public void outputMovieLimitOffMaxMax() {
+        Manager manager = new Manager(3);
+        manager.add("movie1");
+        manager.add("movie2");
+        manager.add("movie3");
+        manager.add("movie4");
+        manager.add("movie5");
+
+        String[] expected = {"movie5", "movie4", "movie3",};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+
     }
 }
